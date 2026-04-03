57cf458794a2af690bf14d07a890940cb8186d5a
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-04-03 16:12:02 +0200

    aggiunto visualizzazione prezzo su schermo

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
+//selezione form ed elementi
 const form = document.querySelector('#ticket-form');
+const card = document.querySelector('#ticket-card');
 
 const kmPercorsi = document.querySelector('#distance');
 const rangeEta = document.querySelector('#age');
+
+const ticketPrice = document.querySelector('#ticket-price');
 
 function submitHandler(event) {
     event.preventDefault();
     const kmValue = kmPercorsi.value;
     const etaValue = rangeEta.value;
 
+    //calcolo prezzo base
     let prezzo = kmValue * 0.21;
-    console.log(prezzo);
 
 
     //verifica scontistica
@@ -20,6 +24,8 @@ function submitHandler(event) {
     }
     console.log(prezzo.toFixed(2));
 
+    ticketPrice.innerHTML = prezzo.toFixed(2);
+    card.classList.remove('d-none');
 }
 
 form.addEventListener('submit', submitHandler);
